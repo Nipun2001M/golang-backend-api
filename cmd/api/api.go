05abeb7319cdcc1df1c0938f87a9d251fb942a36
Nipun2001M <nipun20010328@gmail.com>
2025-04-26 22:58:28 +0530

    health handler created

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 type appication struct {
@@ -13,11 +14,20 @@ type config struct {
 	addresss string
 }
 
-func (app *appication) run() error {
+func (app *appication) mount() *http.ServeMux {
 	mux := http.NewServeMux()
+	mux.HandleFunc("GET /v1/health", app.healthCheckHandler)
+	return mux
+
+}
+
+func (app *appication) run(mux *http.ServeMux) error {
 	srv := &http.Server{
-		Addr:    app.config.addresss,
-		Handler: mux,
+		Addr:         app.config.addresss,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
 	}
 	log.Printf("Server has started at %s", app.config.addresss)
 	return srv.ListenAndServe()
